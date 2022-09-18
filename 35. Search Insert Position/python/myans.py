e57bc06f348e249675139295c272eaa25013c2bd
Leung Wai Lun <leungwilliamlun@gmail.com>
2022-09-18 23:32:04 +0800

    question 35

--- 35. Search Insert Position/python/myans.py
+++ 35. Search Insert Position/python/myans.py
@@ -1,7 +1,23 @@
 from typing import List
 
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
 
-        
-        pass
+nums = [0,3,5,9,12]
+target = 6
+
+
+def searchInsert(nums: List[int], target: int) -> int:
+    upper = len(nums) - 1
+    lower = 0
+
+
+    while lower <= upper:
+        pvivot = lower + (upper - lower) // 2
+        if(nums[pvivot] == target):
+            return pvivot
+        if(nums[pvivot] > target):
+            upper = pvivot - 1
+        else:
+            lower = pvivot + 1
+    return lower
+
+print(searchInsert(nums,target))
